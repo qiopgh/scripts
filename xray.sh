df47615ad637b2fd5ba20379534a9288e912e182
qiopgh <30387662+qiopgh@users.noreply.github.com>
2022-01-30 19:48:46 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -163,7 +163,7 @@ normalizeVersion() {
                 echo "$1"
             ;;
             http*)
-                echo "v1.5.2"
+                echo "v1.5.3"
             ;;
             *)
                 echo "v$1"
