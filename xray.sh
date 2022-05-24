b1abfb847332e6d9dd46ce8564df0a8c8c836f11
qiopgh <30387662+qiopgh@users.noreply.github.com>
2022-05-24 09:45:44 +0800

    Update xray.sh

--- xray.sh
+++ xray.sh
@@ -283,10 +283,10 @@ getData() {
         else
             resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
             res=`echo -n ${resolve} | grep ${IP}`
-            if [[ -z "${res}" ]]; then
-                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
-                colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
-                exit 1
+            #if [[ -z "${res}" ]]; then
+                #colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
+                #colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
+                #exit 1
             fi
         fi
     fi
